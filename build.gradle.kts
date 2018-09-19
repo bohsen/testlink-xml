07c61d65a009519030ecb2be2ed054e5b0e3a74a
Thomas Schmidt <bohsen@gmail.com>
2018-09-19 09:22:49 +0200

    Added fxlauncher for creating executable jar.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,32 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "dk.rn.its"
 version = "1.0-SNAPSHOT"
 
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("no.tornado:fxlauncher-gradle-plugin:1.0.18")
+    }
+}
+
 plugins {
     java
     kotlin("jvm") version "1.2.70"
+    id("no.tornado.fxlauncher") version "1.0.20"
+}
+
+fxlauncher {
+    applicationVendor = "Thomas Bohsen Schmidt"
+    applicationUrl = "https://github.com/bohsen/testlink-xml"
+    applicationMainClass = "app.XsltTransformerApp"
+    acceptDowngrade = false
+    deployTarget = "$buildDir"
 }
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
